e081b7e5ee56003d6a414841c410e23eaa54ef2f
pujielee <riladod22@gmail.com>
2023-04-23 18:16:24 +0800

    Initial Commit

---
+++ DemoFriendList/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DemoFriendList
+//
+//  Created by PujieLee on 2023/4/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DemoFriendList/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DemoFriendList
+//
+//  Created by PujieLee on 2023/4/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
